1729c527af2326308ab0ae326bae6713247a904b
Noor Ma'moun <noorshawar2005@gmail.com>
2026-06-20 09:55:55 +0300

    Add ROS2 package configuration files

---
+++ setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'fireguard_robot'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[],
+    data_files=[
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')
+        ),
+
+        (
+            os.path.join('share', package_name, 'urdf'),
+            glob('urdf/*')
+        ),
+
+        (
+            os.path.join('share', package_name, 'worlds'),
+            glob('worlds/*')
+        ),
+
+        (
+            os.path.join('share', package_name, 'scripts'),
+            glob('scripts/*')
+        ),
+    ],
+
+    install_requires=['setuptools'],
+    zip_safe=True,
+
+    maintainer='Noor',
+    maintainer_email='noor@example.com',
+
+    description='FireGuard Warehouse Robot',
+    license='Apache License 2.0',
+
+    tests_require=['pytest'],
+
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
